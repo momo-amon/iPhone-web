389bd7a07429a1ca3170b9f1ff7602203e41bd5f
momoamon <momoamon147@gmail.com>
2024-01-27 05:07:13 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let img = document.querySelector(".image .img");
+
+function phones(phone) {
+  img.src = phone;
+};
+
+let colors1 = document.querySelector("body");
+
+function colors(color) {
+  colors1.style.backgroundColor = color;
+  ;
+} 
